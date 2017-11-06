6aeb83e7c0a2bd37532720f15ce33527abe2132d
lzq <lvzhiqiang02.@meituan.com>
2017-11-06 18:49:44 +0800

    tmp

--- config/repository.js
+++ config/repository.js
@@ -1,1 +1,1 @@
-/** * Created by lzq on 2017/11/6. */const path = require('path');let dev = {    'repository': '自己定义仓库地址'};
+/** * Created by lzq on 2017/11/6. */const path = require('path');module.exports = {    'repository': '自己定义仓库地址',    'dirPath': path.join(__dirname,'testPath')};
