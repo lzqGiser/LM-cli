ae2a0de81c8b4495ea6a283a41f450ab6b2ac8e9
lzq <1505615071@qq.com>
2017-11-07 16:06:30 +0800

    update babel

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lzq on 2017/11/7.
+ */
+
+require('babel-polyfill');
+require("babel-register")();
+require("./lm-init");
+
---
+++ tmp/template/meta.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lzq on 2017/11/6.
+ */
+module.exports = {
+    prompts:{
+        name:{
+            type: "list"
+            
+        }
+    }
+}
