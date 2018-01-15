e8a0e5d301110e4cfb008a8bb9d3e9a233fc40ed
lzq <1505615071@qq.com>
2018-01-15 19:24:21 +0800

    question start

--- bin/lm-init.js
+++ bin/lm-init.js
@@ -6,9 +6,14 @@ const downGit = require('../lib/downloadGit');
 const userHome = require('user-home');
 const gh = require('../lib/handler');
 const localPath = require('../lib/local-path');
+const codeQuestions = require('../config/questions');
 
 const templatePath = path.join(userHome,'.lmTemplate');
 const lstempout = path.join(process.cwd(), '/lmTemplate');
+
+// 应该先去检测一下，outputPath目录下，有没有内容，防止删除用户目录下的内容
+
+
 
 console.log(`当前工作的路径为：${templatePath}`);
 console.log(`当前输出的路径为：${lstempout}`);
@@ -23,30 +28,37 @@ if(localPath.isLoaclPath(templatePath)){
 * */
 
 function generate(workDir, outputDir){
-    downGit('github:lzqGiser/vue-npm-con',workDir).then(function(msg){
-        console.log(msg);
-        let meta;
-        let metaPath = path.join(workDir,'./meta.js');
 
-        meta = require(path.resolve(metaPath));
+    interAsk(codeQuestions).then(function(answer){
+        console.log(answer.code)
 
-        let questions = meta.questions;
-        let filter = meta.filter;
+        downGit('github:lzqGiser/vue-npm-con',workDir).then(function(msg){
+            console.log(msg);
+            let meta;
+            let metaPath = path.join(workDir,'./meta.js');
 
-        interAsk(questions).then(function(answer){
-            console.log(answer)
+            meta = require(path.resolve(metaPath));
 
-            for(let key in answer){
-                for(let file in filter){
-                    if(key === filter[file] ){
-                        filter[file] = answer[key]
+            let questions = meta.questions;
+            let filter = meta.filter;
+
+            interAsk(questions).then(function(answer){
+                console.log(answer)
+
+                for(let key in answer){
+                    for(let file in filter){
+                        if(key === filter[file] ){
+                            filter[file] = answer[key]
+                        }
                     }
                 }
-            }
-            gh(answer,filter,workDir,outputDir).then(function(result){
-                console.log(`--------------------------------`);
-                console.log(`lm-cli build project ${result}`);
-            })
-        });
+                gh(answer,filter,workDir,outputDir).then(function(result){
+                    console.log(`--------------------------------`);
+                    console.log(`lm-cli build project ${result}`);
+                })
+            });
+        })
+
     })
+
 }
---
+++ config/questions.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lzq on 2018/1/15.
+ */
+
+const codeQuestions = [
+    {
+        type: "input",
+        name: "code",
+        message: "input github address"
+    }
+];
+
+
+module.exports = codeQuestions;
